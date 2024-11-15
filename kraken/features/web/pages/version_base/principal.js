f32f9d6b252251ac4630b926cf89bfc342f7f89b
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-14 20:58:14 -0500

    Modificamos archivo de principal.js para version base.

---
+++ kraken/features/web/pages/version_base/principal.js
@@ -0,0 +1,32 @@
+module.exports = {
+    getTitleAdmin: async function (driver) {
+        let titleField = await driver.$('.gh-nav-menu-details-sitetitle');
+        return titleField.getText();
+    },
+
+    clickPosts: async function (driver) {
+        let clicButton = await driver.$('[href="#/posts/"]');
+        return clicButton.click({force: true});
+    },
+
+    clickPages: async function (driver){
+        let clicButton = await driver.$('[data-test-nav="pages"]');
+        return clicButton.click({force: true});
+    },
+
+    clickTags: async function (driver) {
+        let clicButton = await driver.$('[data-test-nav="tags"]');
+        return clicButton.click({force: true});
+    },
+
+    clickSitePage: async function (driver) {
+        let clicButton = await driver.$('[data-test-nav="site"]');
+        return clicButton.click({force: true});
+    },
+
+    clickMembers: async function (driver) {
+        let clicButton = await driver.$('[data-test-nav="members"]');
+        return clicButton.click({force: true});
+    },
+
+}
