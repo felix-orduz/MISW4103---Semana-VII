491c4c7f235bb4f5a28e39c2ac17c320b40b8324
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-17 16:15:34 -0500

    Modificamos nombre de Escenarios Felix del SS.

--- cypress/e2e/version_base/E00020.cy.js
+++ cypress/e2e/version_base/E00020.cy.js
@@ -18,7 +18,7 @@ describe("Escenarios E2E para Ghost", function () {
       cy.visit(data.baseURL);
       LogIn.logIn(data.email, data.password);
       LogIn.logInButton();
-      cy.screenshot('../../ghost-4.5/E0020-0-BS');
+      cy.screenshot('../../ghost-4.5/E00020-0-BS');
       cy.wait(1000);
     });
   });
@@ -35,44 +35,44 @@ describe("Escenarios E2E para Ghost", function () {
 
 
     cy.wait(3000);
-    cy.screenshot('../../ghost-4.5/E0020-1-BS');
+    cy.screenshot('../../ghost-4.5/E00020-1-BS');
 
     MembersPage.getScreenTitle().should("include.text", "Members");
-    cy.screenshot('../../ghost-4.5/E0020-2-BS');
+    cy.screenshot('../../ghost-4.5/E00020-2-BS');
 
     MembersPage.clickNewMemberButton();
     cy.wait(3000);
-    cy.screenshot('../../ghost-4.5/E0020-3-BS');
+    cy.screenshot('../../ghost-4.5/E00020-3-BS');
 
     MembersPage.fillMemberForm(memberData);
-    cy.screenshot('../../ghost-4.5/E0020-4-BS');
+    cy.screenshot('../../ghost-4.5/E00020-4-BS');
 
     MembersPage.clickSaveButton();
     cy.wait(3000);
-    cy.screenshot('../../ghost-4.5/E0020-5-BS');
+    cy.screenshot('../../ghost-4.5/E00020-5-BS');
 
     MembersPage.goToMembersList();
     cy.wait(2000);
-    cy.screenshot('../../ghost-4.5/E0020-6-BS');
+    cy.screenshot('../../ghost-4.5/E00020-6-BS');
 
     MembersPage.clickMemberByEmail(memberData.email);
-    cy.screenshot('../../ghost-4.5/E0020-7-BS');
+    cy.screenshot('../../ghost-4.5/E00020-7-BS');
 
     // MembersPage.openMemberActions();
-    // cy.screenshot('../../ghost-4.5/E0020-8-BS');
+    // cy.screenshot('../../ghost-4.5/E00020-8-BS');
 
     MembersPage.clickDeleteMember();
     cy.wait(2000);
-    cy.screenshot('../../ghost-4.5/E0020-9-BS');
+    cy.screenshot('../../ghost-4.5/E00020-9-BS');
 
     MembersPage.confirmDeleteMember();
-    cy.screenshot('../../ghost-4.5/E0020-10-BS');
+    cy.screenshot('../../ghost-4.5/E00020-10-BS');
 
     MembersPage.getMembersList().then((membersList) => {
       const member = membersList.find((m) => m.email === memberData.email);
       expect(member).to.be.undefined;
     });
-    cy.screenshot('../../ghost-4.5/E0020-11-BS');
+    cy.screenshot('../../ghost-4.5/E00020-11-BS');
 
   });
 });
