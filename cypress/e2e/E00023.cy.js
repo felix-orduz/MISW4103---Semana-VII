c009484235bec8831de04faf99159dd044069962
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2024-11-20 20:53:41 -0500

    fix import

--- cypress/e2e/E00023.cy.js
+++ cypress/e2e/E00023.cy.js
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { PagesPage, CONTENT } from "../pages/pagesPage";
+import { PagesPage, CONTENT } from "../pages/version_rc/pagesPage";
 
 const PAGE_TITLE = faker.string.alpha(255);
 
