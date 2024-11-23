7a67f632ee40f233f4c338e6e05d0b25876574a7
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2024-11-23 10:19:45 -0500

    add e51

--- cypress/e2e/Metadata.cy.js
+++ cypress/e2e/Metadata.cy.js
@@ -98,4 +98,47 @@ describe('Feature: El usuario admin puede editar la metadata del site.', () => {
         });       
        
     });
+
+
+    it("Escenario 51: Editar meta Title con datos a-priori generados con Mockaroo y una longitud superior a 70 characters", () => {
+        //Given usuario logueado
+        MetadataPage.goToSettings();
+
+        MetadataPage.goToEditMetadata();
+
+        cy.wait(500)
+
+        cy.fixture("metadata.data.apriori.json").then((data) => {
+            let metaTitle = data[4].meta_title;
+            let metaDescription = data[4].meta_description;
+
+            MetadataPage.getMetadataSection()
+                .within(() => {
+                    cy.contains('Edit').click()
+                });
+
+            MetadataPage.getMetadataSection().within(() => {
+                cy.contains('Meta title')
+                    .get('input')
+                    .first()
+                    .clear()
+                    .type(metaTitle);
+
+                //cy.contains('Meta description').get('input').type(metaDescription);
+
+                MetadataPage.getSaveMetadataButton()
+                    .contains('Save')
+                    .click();
+            });
+
+
+            cy.wait(500); 
+            
+            MetadataPage.getMetadataSection().within(() => {
+                MetadataPage.getMetadataPreview()
+                    .should('contain', metaTitle)
+            });
+            
+        });       
+    });
 });
