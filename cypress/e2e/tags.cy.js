05a5e394334eddd8096b10f507b54f151d7f27c3
Daniel Andrade Suarez <d.andrades@uniandes.edu.co>
2024-11-23 11:58:19 -0500

    Creamos todos los escenarios de tags OK.

--- cypress/e2e/tags.cy.js
+++ cypress/e2e/tags.cy.js
@@ -1,11 +1,11 @@
 import { LogIn } from "../pages/logIn";
-import { TagPage } from "../../pages/tagPage";
+import { TagPage } from "../pages/tagPage";
 import { PrincipalPage } from "../pages/principalPage";
-import { da, faker } from "@faker-js/faker";
 import { FakerGenerador } from "../fixtures/generateRandom";
-const data = require("../../fixtures/properties.json");
+const data = require("../fixtures/properties.json");
 
-let dataPoolTag = require("../../fixtures/dataPoolTags.json");
+let dataPoolTag = require("../fixtures/dataPoolTags.json");
+let dataPoolTagsInvalid = require("../fixtures/dataPoolTagsInvalid.json");
 
 Cypress.on("uncaught:exception", (err, runnable) => {
   if (err.message.includes("The play() request was interrupted")) {
@@ -816,6 +816,329 @@ describe("Escenarios E00040 - E00042", function () {
     TagPage.clickDescriptionTag();
 
     //And escribimos la descripción del tag anterior
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //Then Valida Tag publicado en la lista de tags
+    TagPage.busqueda(name, "notClick");
+  });
+});
+
+describe("Escenarios E00043 - E00045", function () {
+  let randomRow;
+
+  beforeEach(() => {
+    cy.fixture("properties.json").then((data) => {
+      //Vistamos sitio de Ghost
+      cy.visit(data.baseURL);
+
+      //Iniciamos sesion
+      LogIn.logIn(data.email, data.password);
+      LogIn.logInButton();
+      cy.screenshot("ss");
+      cy.wait(1000);
+      cy.viewport(1536, 960);
+
+      //Guardamos la apiKey de posts
+      let apiKey = data.apiKey;
+
+      //Realizamos la solicitud a la API de Mockaroo
+      cy.request({
+        method: "GET",
+        url: `https://my.api.mockaroo.com/tags_schema.json?key=${apiKey}`,
+      }).then((response) => {
+        // Verificamos que la respuesta sea exitosa
+        expect(response.status).to.eq(200);
+
+        //Seleccionamos data para la prueba
+        randomRow =
+          response.body[Math.floor(Math.random() * response.body.length)];
+      });
+    });
+  });
+
+  it("E00043 - Eliminar un tag con titulo y descripción (A-priori)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = dataPoolTag[4].tituloTag;
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = dataPoolTag[4].descTag;
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And le de click en la sección de Tags
+    PrincipalPage.clickTags();
+
+    //And Valida Tag publicado en la lista de tags
+    TagPage.lastTagCreated(name, "click");
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en el boton de eliminar
+    TagPage.clickDeleteTag();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //Then elimina el tag
+    TagPage.clickDeleteConfirmTag();
+  });
+
+  it("E00044 - Eliminar un tag con titulo y descripción (Pseudo)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = randomRow.tituloTag;
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = randomRow.descTag;
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And le de click en la sección de Tags
+    PrincipalPage.clickTags();
+
+    //And Valida Tag publicado en la lista de tags
+    TagPage.lastTagCreated(name, "click");
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en el boton de eliminar
+    TagPage.clickDeleteTag();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //Then elimina el tag
+    TagPage.clickDeleteConfirmTag();
+  });
+
+  it("E00045 - Eliminar un tag con titulo y descripción (Aleatorio)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = FakerGenerador.generateRandomData();
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = FakerGenerador.generateRandomData();
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //And le de click en la sección de Tags
+    PrincipalPage.clickTags();
+
+    //And Valida Tag publicado en la lista de tags
+    TagPage.lastTagCreated(name, "click");
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en el boton de eliminar
+    TagPage.clickDeleteTag();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //Then elimina el tag
+    TagPage.clickDeleteConfirmTag();
+  });
+});
+
+describe("Escenarios E00046 - E00048", function () {
+  let randomRow;
+
+  beforeEach(() => {
+    cy.fixture("properties.json").then((data) => {
+      //Vistamos sitio de Ghost
+      cy.visit(data.baseURL);
+
+      //Iniciamos sesion
+      LogIn.logIn(data.email, data.password);
+      LogIn.logInButton();
+      cy.screenshot("ss");
+      cy.wait(1000);
+      cy.viewport(1536, 960);
+
+      //Guardamos la apiKey de posts
+      let apiKey = data.apiKey;
+
+      //Realizamos la solicitud a la API de Mockaroo
+      cy.request({
+        method: "GET",
+        url: `https://my.api.mockaroo.com/tags_invalid_schema.json?key=${apiKey}`,
+      }).then((response) => {
+        // Verificamos que la respuesta sea exitosa
+        expect(response.status).to.eq(200);
+
+        //Seleccionamos data para la prueba
+        randomRow =
+          response.body[Math.floor(Math.random() * response.body.length)];
+      });
+    });
+  });
+
+  it("E00046 - Crear tac con caracteres especiales (A-priori)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = dataPoolTagsInvalid[0].tituloTagSpecial;
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = dataPoolTagsInvalid[0].descTagSpecial;
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //Then Valida Tag publicado en la lista de tags
+    TagPage.busqueda(name, "notClick");
+  });
+
+  it("E00047 - Crear tac con caracteres especiales (Pseudo)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = randomRow.tituloTagSpecial;
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = randomRow.descTagSpecial;
+    TagPage.writeDescriptionTag(description);
+    cy.screenshot("ss");
+
+    //And le de click en el boton guardar
+    TagPage.clickNewTagSave();
+    cy.wait(1000);
+    cy.screenshot("ss");
+
+    //When le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //Then Valida Tag publicado en la lista de tags
+    TagPage.busqueda(name, "notClick");
+  });
+
+  it("E00048 - Crear tac con caracteres especiales (Aleatorio)", function () {
+    //Given le de click en la sección de Tags
+    PrincipalPage.clickTags();
+    cy.screenshot("ss");
+
+    //And el administrador debería ver la página de listado de Tags
+    TagPage.getTitleSection().should("include.text", "Tags");
+
+    //And le de click en el boton New Tag
+    TagPage.clickNewTag();
+    cy.screenshot("ss");
+
+    //And escriba el nombre del tag
+    let name = FakerGenerador.randomSpecialCharacters(4);
+    TagPage.writeNameTag(name);
+    cy.screenshot("ss");
+
+    //And le de click en la descripción del tag
+    TagPage.clickDescriptionTag();
+
+    //And escribimos descripción del tag
+    let description = FakerGenerador.randomSpecialCharacters(10);
     TagPage.writeDescriptionTag(description);
     cy.screenshot("ss");
 
